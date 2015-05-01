aca008efc2bf20ef8007da25c8307e3dc5460ed6
Jason Shiverick <jshiv00@gmail.com>
2015-04-30 19:08:55 -0700

    updated tests and press docs

--- turntable/press.py
+++ turntable/press.py
@@ -308,12 +308,12 @@ def build_collection(df, **kwargs):
     >>> import pandas as pd
     >>> import turntable
     >>>
-    >>> df = pd.DataFrame({'Artist':"""Michael Jackson, Pink Floyd, Whitney Houston, Meat Loaf, Eagles, Fleetwood Mac, Bee Gees, AC/DC""".split(', '),f'Album' :"""Thriller, The Dark Side of the Moon, The Bodyguard, Bat Out of Hell, Their Greatest Hits (1971–1975), Rumours, Saturday Night Fever, Back in Black""".split(', ')})
-    >>>
+    >>> df = pd.DataFrame({'Artist':"""Michael Jackson, Pink Floyd, Whitney Houston, Meat Loaf, Eagles, Fleetwood Mac, Bee Gees, AC/DC""".split(', '),
+    >>> 'Album' :"""Thriller, The Dark Side of the Moon, The Bodyguard, Bat Out of Hell, Their Greatest Hits (1971-1975), Rumours, Saturday Night Fever, Back in Black""".split(', ')})
     >>> collection = turntable.press.build_collection(df, my_favorite_record = 'nevermind')
-    >>>
     >>> record = collection[0]
-    >>>print record.series
+    >>> print record.series
+
     '''
     print 'Generating the Record Collection...'
     print ' '
