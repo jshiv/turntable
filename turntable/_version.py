ffa721a5e864c4b9b53b467abd3970362e38b557
Jason Shiverick <jshiv00@gmail.com>
2015-05-15 17:47:01 -0700

    testing versioning

--- turntable/_version.py
+++ turntable/_version.py
@@ -35,7 +35,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440-old"
+    cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "turntable-"
     cfg.versionfile_source = "turntable/_version.py"
